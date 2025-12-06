01b4b1eebcadeba1e73e7fa82b2ed5af01e4cb83
Vincent Verhoeven <verhoevenv@gmail.com>
2025-12-06 11:40:30 +0100

    Day 6 part 2

--- lib/day6.ex
+++ lib/day6.ex
@@ -9,8 +9,17 @@ defmodule Day6 do
     |> Enum.sum()
   end
 
-  def part2(_input) do
-    "todo"
+  def part2(input) do
+    grid = to_grid(input)
+    space_columns = find_space_columns(grid)
+    num_starts = [0] ++ Enum.map(space_columns, fn x -> x + 1 end)
+    num_ends = Enum.map(space_columns, fn x -> x - 1 end) ++ [grid[:size_x] - 1]
+    number_ranges = Enum.zip(num_starts, num_ends)
+
+    number_ranges
+    |> Enum.map(&read_numbers_and_ops_from_columns(&1, grid))
+    |> Enum.map(&calculate/1)
+    |> Enum.sum()
   end
 
   defp calculate(nums_and_op) do
@@ -23,4 +32,59 @@ defmodule Day6 do
   defp parse_op("*"), do: &Enum.product/1
   defp parse_op("+"), do: &Enum.sum/1
 
+  defp to_grid(input) do
+    lines = String.split(input, "\n")
+    size_y = length(lines)
+    size_x = String.length(List.first(lines))
+
+    cells =
+      for {line, y} <- Enum.with_index(lines),
+          {elem, x} <- Enum.with_index(String.graphemes(line)),
+          into: %{} do
+        {{x, y}, elem}
+      end
+
+    %{
+      cells: cells,
+      size_x: size_x,
+      size_y: size_y
+    }
+  end
+
+  defp find_space_columns(grid) do
+    %{cells: cells, size_x: size_x, size_y: size_y} = grid
+
+    all_spaces? = fn col_number ->
+      0..(size_y - 1)
+      |> Enum.map(&cells[{col_number, &1}])
+      |> Enum.all?(&(&1 == " "))
+    end
+
+    0..(size_x - 1)
+    |> Enum.filter(all_spaces?)
+  end
+
+  defp read_numbers_and_ops_from_columns(range, grid) do
+    %{cells: cells, size_y: size_y} = grid
+    {from, to} = range
+
+    numbers_str = to..from//-1 |> Enum.map(&read_column_number(&1, grid))
+
+    op_str =
+      from..to
+      |> Enum.map(&cells[{&1, size_y - 1}])
+      |> Enum.join("")
+      |> String.trim()
+
+    numbers_str ++ [op_str]
+  end
+
+  defp read_column_number(col, grid) do
+    %{cells: cells, size_y: size_y} = grid
+
+    0..(size_y - 2)
+    |> Enum.map(&cells[{col, &1}])
+    |> Enum.join("")
+    |> String.trim()
+  end
 end
--- test/day6_test.exs
+++ test/day6_test.exs
@@ -2,20 +2,22 @@ defmodule Day6Test do
   use ExUnit.Case
 
   def example_input do
-    """
-    123 328  51 64
-     45 64  387 23
-      6 98  215 314
-    *   +   *   +
-    """
-    |> String.trim_trailing("\n")
+    # formatter destroys spaces at end of multiline string,
+    # we have to be a bit more careful here :)
+    [
+      "123 328  51 64 ",
+      " 45 64  387 23 ",
+      "  6 98  215 314",
+      "*   +   *   +  "
+    ]
+    |> Enum.join("\n")
   end
 
   test "part1" do
-    assert Day6.part1(example_input()) == 4277556
+    assert Day6.part1(example_input()) == 4_277_556
   end
 
   test "part2" do
-    assert Day6.part2(example_input()) == "done"
+    assert Day6.part2(example_input()) == 3_263_827
   end
 end
